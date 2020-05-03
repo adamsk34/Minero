6225b8a02f1b037487e4a6b65abc5a6048cd857d
Kevin Adams <adamsk34@myumanitoba.ca>
2020-05-03 13:07:38 -0500

    Added license to art.js

--- client/art.js
+++ client/art.js
@@ -1,4 +1,21 @@
+/**
+    Copyright 2020 Kevin Adams ©
 
+    This file is a part of Minero
+
+    Minero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Minero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Minero.  If not, see <https://www.gnu.org/licenses/>.
+*/
 let ctx = document.getElementById("ctx").getContext("2d");
 ctx.font = "30px Arial";
 
